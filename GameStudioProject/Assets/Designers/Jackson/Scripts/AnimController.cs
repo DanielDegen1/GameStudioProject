11084a080392a776b1bcfa540ab27779f23b186f
jackosullivan88 <jackosullivan88@gmail.com>
2021-10-05 22:14:02 -0400

    Arm animations in progress

---
+++ GameStudioProject/Assets/Designers/Jackson/Scripts/AnimController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    public GameObject bow;
+    public GameObject arrow;
+    public Transform bowParent;
+    public Transform arrowParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnBowandArrow() {
+        
+    }
+}
