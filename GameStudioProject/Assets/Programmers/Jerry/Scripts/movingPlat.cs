1796e92e1361847fb6719695bc640a971a232282
Gerald-R-M <gerald.monnecka1@marist.edu>
2021-10-12 19:15:11 -0400

    Moving platforms fixed
    Don't make points that you want to remain stationary the children of a moving object :)

--- GameStudioProject/Assets/Programmers/Jerry/Scripts/movingPlat.cs
+++ GameStudioProject/Assets/Programmers/Jerry/Scripts/movingPlat.cs
@@ -4,54 +4,63 @@ using UnityEngine;
 
 public class movingPlat : MonoBehaviour
 {
-    /*
-    public float positiveXOffset;
-    public float positiveYOffset;
-    public float positiveZOffset;
-    public float negativeXOffset;
-    public float negativeYOffset;
-    public float negativeZOffset;
-    */
+
     public GameObject[] pivotPoints;
+    public float platSpeed = 1; //speed the platform will move
+    public float delayTimer; //how long until platform moves again after stopping
+
+    private float tolerance;
     private int pivotCounter = 0;
     private Vector3 currentTarget;
-    private bool DEBUG = true; //if you dont want the debug.log spammed set this to false
+    private bool DEBUG = false; //if you dont want the debug.log spammed set this to false
+    private float delayStart;
     // Start is called before the first frame update
     void Start()
     {
         currentTarget = pivotPoints[0].transform.position;
-        if (DEBUG == true)
-        {
-            Debug.Log("Moving Platform script attached :)");
-
-        }
+        tolerance = platSpeed * Time.deltaTime;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, currentTarget, 0.1f);
-        if (transform.position == currentTarget) //once the platform has reached its target 
+        if(transform.position != currentTarget) //move the platform towards the target
+        {
+            movePlatform();
+        }
+        else //once the platform has reached its target 
         {
             if (DEBUG == true)
             {
                 Debug.Log("Platform has reached pivot point #" + pivotCounter);
             }
-            pivotCounter++; //increment the pivot tracker
             updateTarget(); //set currentTarget = the position of the next pivot point
         }
     }
     private void updateTarget()
     {
-        if (DEBUG == true)
+        if(Time.time-delayStart > delayTimer)
         {
-            Debug.Log("Target is being updated to pivot position #" + pivotCounter);
+            NextPlatform();
         }
-        if(pivotCounter >= pivotPoints.Length - 1)
+    }
+    private void NextPlatform()
+    {
+        pivotCounter++;
+        if (pivotCounter >= pivotPoints.Length)
         {
-            Debug.Log("Target is now the first pivot point");
             pivotCounter = 0;
         }
         currentTarget = pivotPoints[pivotCounter].transform.position;
+    }
+    private void movePlatform()
+    {
+        Vector3 heading = currentTarget - transform.position;
+        transform.position += (heading / heading.magnitude) * platSpeed * Time.deltaTime;
+        if(heading.magnitude < tolerance)
+        {
+            transform.position = currentTarget;
+            delayStart = Time.time;
+        }
     }
 }
