a215d765291694b3f5dcd678b90e0e66a5d138ba
Gerald-R-M <gerald.monnecka1@marist.edu>
2021-10-18 16:07:37 -0400

    main menu music
    Changed the main menu music to not restart whenever you click a button on the menu

--- GameStudioProject/Assets/Designers/Matt M/Scripts/Menuscript.cs
+++ GameStudioProject/Assets/Designers/Matt M/Scripts/Menuscript.cs
@@ -6,17 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class Menuscript : MonoBehaviour
 {
+    public GameObject musicController;
     public void ButtonMainMenu(){
         SceneManager.LoadScene("MenuScene");
+        DontDestroyOnLoad(musicController.gameObject);
+
     }
     public void ButtonStart() {
         SceneManager.LoadScene("GameScene");
+        Destroy(musicController.gameObject);
     }
     public void ButtonOptions() {
         SceneManager.LoadScene("OptionsScene");
+        DontDestroyOnLoad(musicController.gameObject);
     }
     public void ButtonCredits() {
         SceneManager.LoadScene("CreditsScene");
+        DontDestroyOnLoad(musicController.gameObject);
     }
     public void ButtonQuit() {
         Application.Quit();
