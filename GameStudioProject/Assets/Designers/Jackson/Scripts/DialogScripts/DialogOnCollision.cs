166c675aec79e67e5f21287d83f10106e1f62f2d
jackosullivan88 <jackosullivan88@gmail.com>
2021-10-28 12:27:27 -0400

    More dialog work

---
+++ GameStudioProject/Assets/Designers/Jackson/Scripts/DialogScripts/DialogOnCollision.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity
+{
+    public class DialogOnCollision : MonoBehaviour
+    {
+        public string talkToNode = "";
+
+        public YarnProgram scriptToLoad;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            DialogueRunner dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+            dialogueRunner.Add(scriptToLoad);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                if (FindObjectOfType<DialogueRunner>().IsDialogueRunning)
+                {
+                    FindObjectOfType<DialogueRunner>().Stop();
+                }
+                FindObjectOfType<DialogueRunner>().StartDialogue(talkToNode);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
